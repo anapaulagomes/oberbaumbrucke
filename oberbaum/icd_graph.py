8beea0a2d8c8409e72e5bc96f274e6c2de15744d
Ana Paula Gomes <apgomes88@gmail.com>
2025-04-11 13:24:32 +0200

    Add WHO ICD-10 2019 codes

--- oberbaum/icd_graph.py
+++ oberbaum/icd_graph.py
@@ -17,12 +17,16 @@ class ICDGraph(ABC):
     def __post_init__(self):
         self.add_root_node()
         self.add_chapters()
-
-    def add_root_node(self):
-        self.graph.add_node(self._root_node)
+        self.add_codes()
 
     def add_chapters(self):
         raise NotImplementedError("Version Graph class must implement this method.")
+
+    def add_codes(self):
+        raise NotImplementedError("Version Graph class must implement this method.")
+
+    def add_root_node(self):
+        self.graph.add_node(self._root_node)
 
     def chapters(self):
         return nx.descendants(self.graph, self._root_node)
@@ -32,18 +36,45 @@ class ICDGraph(ABC):
 class WHOICDGraph(ICDGraph):
     """Class for representing the WHO ICD structure as a graph.
 
-    The version implemented here is the 2019 and the files can be
-    downloaded from: https://www.who.int/classifications/icd/en/
+    The version implemented here is the 2019.
+    Files for download: https://icdcdn.who.int/icd10/meta/icd102019enMeta.zip
+    ICD-10 metadata Format: https://icdcdn.who.int/icd10/metainfo.html
+    Guidelines: https://icd.who.int/browse10/Content/statichtml/ICD10Volume2_en_2019.pdf
     """
 
     version_name: str = "icd-10-who"
 
     def add_chapters(self):
+        """The instructions mention 21 chapters but the file has 22."""
         chapters_file = Path(self.files_dir) / "icd102019syst_chapters.txt"
         for line in chapters_file.read_text().splitlines():
             chapter_code, chapter_name = line.split(";", 1)
             self.graph.add_node(chapter_code, name=chapter_name)
             self.graph.add_edge(self._root_node, chapter_code)
+
+    def add_codes(self):
+        """Add all codes to the graph.
+
+        Fields:
+            hierarchy_level;tree_place;terminal_node_type;chapter_number;
+            first_3_block_code;code;code_without_asterisk;code_without_dot;
+            full_title;title;subtitle;code_type;mortality1;
+            mortality2;mortality3;mortality4;morbidity_list
+        """
+        codes_file = Path(self.files_dir) / "icd102019syst_codes.txt"
+        for line in codes_file.read_text().splitlines():
+            fields = line.split(";")
+            data = {
+                "chapter_number": fields[3],
+                "block": fields[4],
+                "formatted_code": fields[5],
+                "code": fields[7],  # code without dot
+                "full_title": fields[8],
+                "title": fields[9],
+                "subtitle": fields[10],
+            }
+            self.graph.add_node(data["code"], name=data["full_title"], **data)
+            self.graph.add_edge(data["block"], data["code"])
 
 
 def get_graph(version: str, files_dir: str) -> ICDGraph:
--- tests/test_icd_graph.py
+++ tests/test_icd_graph.py
@@ -5,7 +5,7 @@ from oberbaum.icd_graph import ICDGraph, get_graph, WHOICDGraph
 
 
 @pytest.fixture
-def chapters_file_dir(tmp_path):
+def icd_file_dir(tmp_path):
     chapters_file = tmp_path / "icd102019syst_chapters.txt"
     chapters_file.write_text(
         "01;Certain infectious and parasitic diseases\n"
@@ -14,25 +14,44 @@ def chapters_file_dir(tmp_path):
         "04;Endocrine, nutritional and metabolic diseases\n"
         "05;Mental and behavioural disorders\n"
     )
+    codes_file = tmp_path / "icd102019syst_codes.txt"
+    codes_file.write_text(
+        "3;N;X;01;A00;A00.-;A00;A00;Cholera;Cholera;;;001;4-002;3-003;2-001;1-002\n"
+        "4;T;X;01;A00;A00.0;A00.0;A000;Cholera due to Vibrio cholerae 01, biovar cholerae;Cholera;Cholera due to Vibrio cholerae 01, biovar cholerae;;001;4-002;3-003;2-001;1-002\n"
+        "4;T;X;01;A00;A00.1;A00.1;A001;Cholera due to Vibrio cholerae 01, biovar eltor;Cholera;Cholera due to Vibrio cholerae 01, biovar eltor;;001;4-002;3-003;2-001;1-002\n"
+    )
     return str(tmp_path)
 
 
 class TestWHOICD10Graph:
-    def test_create_who_icd10_graph(self, chapters_file_dir):
-        graph = WHOICDGraph(files_dir=chapters_file_dir)
+    def test_create_who_icd10_graph(self, icd_file_dir):
+        graph = WHOICDGraph(files_dir=icd_file_dir)
         assert graph.version_name == "icd-10-who"
         assert isinstance(graph.graph, nx.DiGraph)
 
-    def test_get_all_chapters(self, chapters_file_dir):
-        graph = WHOICDGraph(files_dir=chapters_file_dir)
+    def test_get_all_chapters(self, icd_file_dir):
+        graph = WHOICDGraph(files_dir=icd_file_dir)
         chapters = graph.chapters()
         assert isinstance(chapters, set)
         assert len(chapters) == 5
 
+    def test_add_chapters(self, icd_file_dir):
+        graph = WHOICDGraph(files_dir=icd_file_dir)
+        assert len(graph.graph.nodes) == 6  # root and 5 chapters
+
+    def test_add_codes(self, icd_file_dir):
+        graph = WHOICDGraph(files_dir=icd_file_dir)
+
+        assert graph.graph.has_node("A00")
+        assert graph.graph.has_node("A000")
+        assert graph.graph.has_node("A001")
+        assert graph.graph.has_edge("A00", "A000")
+        assert graph.graph.has_edge("A00", "A001")
+
 
 class TestGetGraph:
-    def test_get_graph_by_name(self, chapters_file_dir):
-        graph = get_graph("icd-10-who", chapters_file_dir)
+    def test_get_graph_by_name(self, icd_file_dir):
+        graph = get_graph("icd-10-who", icd_file_dir)
         assert isinstance(graph, ICDGraph)
         assert isinstance(graph, WHOICDGraph)
         assert graph.version_name == "icd-10-who"
