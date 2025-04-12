794b688fa9db6d96a193bf486ae2fb71fb19d48c
Ana Paula Gomes <apgomes88@gmail.com>
2025-04-12 20:41:34 +0200

    Load fixture according to version

--- tests/icd_graph/test_icd_graph.py
+++ tests/icd_graph/test_icd_graph.py
@@ -5,14 +5,16 @@ from oberbaum.icd_graph import ICDGraph, get_graph, WHOICDGraph, CID10Graph
 
 class TestGetGraph:
     @pytest.mark.parametrize(
-        "version, expected_class",
+        "version, expected_class, file_dir",
         [
-            ("icd-10-who", WHOICDGraph),
-            ("cid-10-bra", CID10Graph),
+            ("icd-10-who", WHOICDGraph, "icd10_who_file_dir"),
+            ("cid-10-bra", CID10Graph, "cid10_bra_file_dir"),
         ],
     )
-    def test_get_graph_by_name(self, icd10_who_file_dir, version, expected_class):
-        graph = get_graph(version, icd10_who_file_dir)
+    def test_get_graph_by_name(self, version, expected_class, file_dir, request):
+        file_dir = request.getfixturevalue(file_dir)
+        graph = get_graph(version, file_dir)
+
         assert isinstance(graph, ICDGraph)
         assert isinstance(graph, expected_class)
         assert graph.version_name == version
