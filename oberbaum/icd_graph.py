adf687cabeaa7ef32c5f499226fbbfd8f25548b2
Ana Paula Gomes <apgomes88@gmail.com>
2025-04-15 13:20:20 +0200

    Connect blocks and sub-blocks and remove none that prevent exporting the GML file

--- oberbaum/icd_graph.py
+++ oberbaum/icd_graph.py
@@ -102,8 +102,42 @@ class ICDGraph(ABC):
         self.graph.add_node(block_name, **data)
         del data["is_block"]
         self._blocks[block_name] = data
+
+        parent_block = self.find_parent_block(block_name)
+        if parent_block:
+            if chapter_code:
+                self.connect_chapter_block(
+                    chapter_code, parent_block
+                )  # chapter and block
+            self.connect_blocks(parent_block, block_name)  # block and sub-block
+            return
         if chapter_code:
             self.connect_chapter_block(chapter_code, block_name)
+
+    def find_parent_block(self, block):
+        """Find the parent of a given block.
+
+        A block may be a child of a chapter or another block."""
+        block_data = self.get(block)
+        block_start_letter = block_data["start"][0]
+        block_end_letter = block_data["end"][0]
+        for current_block, current_data in self.blocks(data=True):
+            if (
+                current_data["start"][0] == block_start_letter
+                and current_data["end"][0] == block_end_letter
+            ):
+                match_start = int(block_data["start"][1:]) >= int(
+                    current_data["start"][1:]
+                )
+                match_end = int(current_data["end"][1:]) >= int(block_data["end"][1:])
+
+                if block != current_block and (match_start and match_end):
+                    return current_block
+        return
+
+    def get(self, node):
+        """Get the data for a given element in the graph."""
+        return self.graph.nodes[node]
 
     def add_code(
         self,
@@ -133,8 +167,21 @@ class ICDGraph(ABC):
         self.graph.add_edge(three_char_category, code)
 
     def connect_chapter_block(self, chapter_code, block_name):
-        """Create the edge between chapter and block."""
+        """Create the edge between chapter and block.
+
+        If the chapter code is not present in the block, it will be added to the block node
+        for convenience."""
+        if not self.graph.nodes[block_name].get(chapter_code):
+            # FIXME update_block()
+            self.graph.nodes[block_name]["chapter_code"] = chapter_code
+            self._blocks[block_name]["chapter_code"] = chapter_code
+
         self.graph.add_edge(chapter_code, block_name)
+
+    def connect_blocks(self, block, sub_block):
+        """Create the edge between chapter and block."""
+        self.graph.add_edge(block, sub_block)
+        self._blocks[sub_block]["main_block"] = block
 
     def chapters(self, roman_numerals=False, data=False):
         if data:
@@ -205,8 +252,20 @@ class ICDGraph(ABC):
 
     def export(self):
         gml_file = f"{self.version_name}.gml"
-        nx.write_gml(self.graph, gml_file)
+        graph_copy = self.graph.copy()
+        graph_copy = self._from_none_to_empty(graph_copy, graph_copy.nodes(data=True))
+        nx.write_gml(graph_copy, gml_file)
         return gml_file
+
+    def _from_none_to_empty(self, a_graph, data_dict):
+        """Convert all None values to empty strings in a dictionary."""
+        for item, data in data_dict:
+            if item == self._root_node:
+                continue
+            for k, v in data.items():
+                if v is None:
+                    data[k] = ""
+        return a_graph
 
     def find_chapter(self, code):
         """Find the chapter for a given code.
@@ -215,15 +274,16 @@ class ICDGraph(ABC):
         Use this method to find the chapter for a given code."""
         return self._find_in(self.chapters(data=True), code)
 
-    def find_block(self, code):
+    def find_block(self, code, include_subblocks=False):
         """Find the block for a given code.
 
         Every block has a start and end category code.
         Use this method to find the block for a given code."""
-        return self._find_in(self.blocks(data=True), code)
+        return self._find_in(self.blocks(data=True), code, include_subblocks)
 
-    def _find_in(self, data_set: dict, code: str):
+    def _find_in(self, data_set: dict, code: str, return_all=False):
         letter = code[0]
+        found = []
         for item, data in data_set:
             start = data.get("start")
             end = data.get("end")
@@ -238,18 +298,34 @@ class ICDGraph(ABC):
 
             if letter == start_letter == end_letter:
                 if start_number <= number <= end_number:
-                    return item
+                    if not return_all:
+                        return item
+
+                    found.append(item)
             else:
                 if letter == start_letter:
                     if number >= start_number:
-                        return item
+                        if not return_all:
+                            return item
+
+                        found.append(item)
                 elif letter == end_letter:
                     if number <= end_number:
-                        return item
+                        if not return_all:
+                            return item
+
+                        found.append(item)
                 else:
                     if start_letter < letter < end_letter:
-                        return item
-        return
+                        if not return_all:
+                            return item
+
+                        found.append(item)
+
+        if not return_all:
+            return None
+
+        return found
 
 
 @dataclass
@@ -370,12 +446,28 @@ class CID10Graph(ICDGraph):
             description = line["DESCRICAO"]
             title = line["DESCRABREV"]
             chapter = self.find_chapter(code)
-            block = self.find_block(code)
+            blocks = self.find_block(code, include_subblocks=True)
+            block = blocks[0]
+            sub_block = block
+            if len(blocks) > 1:
+                sub_block = blocks[1]
             three_char_category = code[:3]  # also from CID-10-CATEGORIAS.CSV
 
-            self.add_code(code, chapter, block, three_char_category, description, title)
+            # FIXME three cat nao tem dados
+            # FIXME sub_block nao tem chapter code
+
+            self.add_code(
+                code,
+                chapter,
+                block,
+                three_char_category,
+                description,
+                title,
+                sub_block=sub_block,
+            )
             self.connect_chapter_block(chapter, block)
-            self.connect_block_three_char_category(block, three_char_category)
+            # block and sub-block are already connected due to groups' file structure
+            self.connect_block_three_char_category(sub_block, three_char_category)
             self.connect_three_char_category_code(three_char_category, code)
 
 
--- tests/icd_graph/test_cid10_graph.py
+++ tests/icd_graph/test_cid10_graph.py
@@ -26,21 +26,46 @@ class TestCID10Graph:
 
     @pytest.mark.integration
     @pytest.mark.parametrize(
-        "chapter,code,message",
+        "code,chapter,block,message",
         [
-            ("1", "A01", "Different but close letters e.g. A00-B99"),
-            ("3", "D89", "Same letters but different intervals e.g. D50-D89"),
-            ("10", "J101", "Same letters, all intervals e.g. J09-J18"),
-            ("20", "W010", "Different letters but within interval e.g. V01-Y98"),
-            ("2", "C490", "Same letters but different intervals e.g. C00;D48"),
-            ("20", "X850", "Different letters but within interval e.g. X85-Y09"),
-            (None, "Y99", "Nonexistent chapter"),
+            ("A01", "1", "A00-A09", "Different but close letters e.g. A00-B99"),
+            (
+                "D89",
+                "3",
+                "D80-D89",
+                "Same letters but different intervals e.g. D50-D89",
+            ),
+            ("J101", "10", "J09-J18", "Same letters, all intervals e.g. J09-J18"),
+            (
+                "W010",
+                "20",
+                "V01-X59",
+                "Different letters but within interval e.g. V01-Y98",
+            ),
+            (
+                "C490",
+                "2",
+                "C00-C97",
+                "Same letters but different intervals e.g. C00;D48",
+            ),
+            (
+                "X850",
+                "20",
+                "X85-Y09",
+                "Different letters but within interval e.g. X85-Y09",
+            ),
+            ("Y99", None, None, "Nonexistent chapter"),
         ],
     )
-    def test_find_chapter(self, real_cid10_bra_file_dir, chapter, code, message):
+    def test_find_chapter_and_block(
+        self, real_cid10_bra_file_dir, code, chapter, block, message
+    ):
         graph = CID10Graph(files_dir=real_cid10_bra_file_dir)
+        found_chapter = graph.find_chapter(code)
+        found_block = graph.find_block(code)
 
-        assert graph.find_chapter(code) == chapter, message
+        assert found_chapter == chapter, message
+        assert found_block == block, message
 
     def test_codes(self, cid10_bra_file_dir):
         graph = CID10Graph(files_dir=cid10_bra_file_dir)
@@ -60,5 +85,35 @@ class TestCID10Graph:
 
         assert len(graph.chapters()) == 22
         assert len(graph.blocks()) == 275
-        assert len(graph.three_char_codes()) == 2045
-        assert len(graph.four_char_codes()) == 12188
+        assert len(graph.three_char_codes()) == 2045  # 1626
+        assert len(graph.four_char_codes()) == 12188  # 8859
+        assert all(list(data["chapter_code"] for _, data in graph.blocks(data=True)))
+
+    def test_handle_sublocks(self, tmp_path):
+        blocks_file = tmp_path / "CID-10-GRUPOS.CSV"
+        blocks_file.write_text(
+            "CATINIC;CATFIM;DESCRICAO;DESCRABREV;\n"
+            "T20;T32;Queimaduras e corrosões;Queimaduras e corrosões;\n"
+            "T20;T25;Queimaduras e corrosões da superfície externa do corpo, especificadas por local;Queimad e corros superf ext corpo, espec p/local;\n"
+            "T26;T28;Queimaduras e corrosões limitadas ao olho e aos órgãos internos;Queimad e corrosões limit olho e órgãos internos;\n"
+            "T29;T32;Queimaduras e corrosões de múltiplas regiões e de regiões não especificadas do corpo;Queimad e corrosões múlt reg e reg não espec corpo;\n"
+        )
+        chapters_file = tmp_path / "CID-10-CAPITULOS.CSV"
+        chapters_file.write_text(
+            "NUMCAP;CATINIC;CATFIM;DESCRICAO;DESCRABREV;\n"
+            "19;S00;T98;Capítulo XIX - Lesões, envenenamento e algumas outras conseqüências de causas externas;XIX. Lesões enven e alg out conseq causas externas;\n"
+        )
+        subcategories_file = tmp_path / "CID-10-SUBCATEGORIAS.CSV"
+        subcategories_file.write_text(
+            "SUBCAT;CLASSIF;RESTRSEXO;CAUSAOBITO;DESCRICAO;DESCRABREV;REFER;EXCLUIDOS;\n"
+            "T200;;;;Queimadura da cabeça e do pescoço, grau não especificado;T20.0 Queim da cabeca e do pescoco grau NE;;;\n"
+            "T201;;;N;Queimadura de primeiro grau da cabeça e do pescoço;T20.1 Queim de 1.grau da cabeca e pescoco;;;\n"
+            "T210;;;;Queimadura do tronco, grau não especificado;T21.0 Queim do tronco grau NE;;;\n"
+            "T211;;;N;Queimadura de primeiro grau do tronco;T21.1 Queim de 1.grau do tronco;;;\n"
+        )
+        graph = CID10Graph(files_dir=str(tmp_path))
+
+        assert graph.graph.has_edge("T20-T32", "T20-T25")  # block and sub-blocks
+        assert graph.graph.has_edge("T20-T32", "T26-T28")
+        assert graph.graph.has_edge("T20-T32", "T29-T32")
+        assert graph.graph.has_edge("19", "T20-T32")  # chapter and block
