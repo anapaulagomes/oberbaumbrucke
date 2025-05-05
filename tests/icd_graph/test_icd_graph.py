e8ab1d2c62a6d71674016277ac154d97d497c699
Ana Paula Gomes <apgomes88@gmail.com>
2025-05-05 21:32:57 +0200

    Add test to is_code

--- tests/icd_graph/test_icd_graph.py
+++ tests/icd_graph/test_icd_graph.py
@@ -1,4 +1,5 @@
 from oberbaum.icd_graph.graphs.brazil import CID10Graph
+from oberbaum.icd_graph.graphs.who import WHOICDGraph
 
 
 class TestLoadGraphFromFile:
@@ -8,3 +9,13 @@ class TestLoadGraphFromFile:
         assert graph.chapters()
         assert graph.blocks()
         assert graph.codes()
+
+    def test_is_code(self, real_icd10_who_file_dir):
+        graph = WHOICDGraph(files_dir=real_icd10_who_file_dir)
+
+        assert graph.is_code("1") is False
+        assert graph.is_code("A92-A99") is False
+        assert graph.is_code("A92") is False
+        assert graph.is_code("A") is False
+        assert graph.is_code("J10") is False
+        assert graph.is_code("A929") is True
