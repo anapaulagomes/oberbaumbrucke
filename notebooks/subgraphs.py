5b82c62b283897a104725565b3e8c55261cd6b94
Ana Paula Gomes <apgomes88@gmail.com>
2025-05-23 12:58:58 +0200

    Improve visualization

--- notebooks/subgraphs.py
+++ notebooks/subgraphs.py
@@ -21,40 +21,59 @@ def _(mo):
     return (upload,)
 
 
+@app.function
+# expected: subgraph-icd-10-who-S361-include-children.gml
+# expected: subgraph-icd-10-who-S361.gml
+
+def get_version_target_node(filename):
+    filename = (
+        filename.replace("subgraph-", "")
+        .replace("-include-children.gml", "")
+        .replace(".gml", "")
+    )
+    version, target_node = filename.rsplit("-", 1)
+    return version, target_node
+
+
 @app.cell
-def _(nx, upload):
+def _(nx):
     def parse_gml(content):
         content_str = content.decode("utf-8")
         G = nx.parse_gml(content_str)
         return nx.DiGraph(G) if not isinstance(G, nx.DiGraph) else G
 
-    trees = {}
-    for index, gml_file in enumerate(upload.value):
-        version = upload.name(index)
-        content = upload.value[index].contents
-        trees[version] = parse_gml(content)
+    def get_trees_from(upload):
+        trees = {}
+        target_node = None
+        for index, gml_file in enumerate(upload.value):
+            version = upload.name(index)
+            version, target_node = get_version_target_node(upload.name(index))
+            content = upload.value[index].contents
+            trees[version] = parse_gml(content)
+        return trees, target_node
 
-    return (trees,)
+    return (get_trees_from,)
+
+
+@app.function
+def get_hierarchical_pos(G):
+    prog = "dot"
+    try:
+        from networkx.drawing.nx_agraph import graphviz_layout
+
+        return graphviz_layout(G, prog=prog)
+    except ImportError:
+        try:
+            from networkx.drawing.nx_pydot import graphviz_layout
+
+            return graphviz_layout(G, prog=prog)
+        except ImportError:
+            raise ImportError("You need to install pygraphviz or pydot and Graphviz")
 
 
 @app.cell
-def _(go, make_subplots, trees):
-    def get_hierarchical_pos(G):
-        try:
-            from networkx.drawing.nx_agraph import graphviz_layout
-
-            return graphviz_layout(G, prog="dot")
-        except ImportError:
-            try:
-                from networkx.drawing.nx_pydot import graphviz_layout
-
-                return graphviz_layout(G, prog="dot")
-            except ImportError:
-                raise ImportError(
-                    "You need to install pygraphviz or pydot and Graphviz"
-                )
-
-    def plot_trees(trees):
+def _(go, make_subplots):
+    def plot_trees(trees, target_node):
         fig = make_subplots(
             rows=2,
             cols=2,
@@ -100,10 +119,10 @@ def _(go, make_subplots, trees):
                     y=node_y,
                     mode="markers+text",
                     text=[
-                        f"{n} - {data.get('title', '')}"
+                        f"{n} - {data.get('description', '')}"
                         for n, data in G.nodes(data=True)
                     ],
-                    marker=dict(color="lightblue", size=10),
+                    marker=dict(color="blue", size=10),
                     textposition="top center",
                     showlegend=False,
                 ),
@@ -113,15 +132,31 @@ def _(go, make_subplots, trees):
 
         fig.update_layout(
             height=700,
-            width=700,
-            margin=dict(l=20, r=20, t=40, b=40),
-            # title_text="ICD-10 versions"
+            width=1400,
+            margin=dict(l=20, r=20, t=100, b=40),
+            title_text="ICD-10 versions",
         )
         fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
         return fig
 
-    plot_trees(trees)
+    return (plot_trees,)
 
+
+@app.cell
+def _(get_trees_from, plot_trees, upload):
+    trees, target_node = get_trees_from(upload)
+
+    plot_trees(trees, target_node)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
     return
 
 
