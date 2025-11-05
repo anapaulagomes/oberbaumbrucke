a202d186ef3e9ee5548627b3857cfc418e79671d
Ana Paula Gomes <apgomes88@gmail.com>
2025-11-05 12:22:15 +0100

    Calculate metrics and plot

--- notebooks/mappings_validation.py
+++ notebooks/mappings_validation.py
@@ -21,8 +21,10 @@ def _(get_connection):
 
 
 @app.cell
-def _(con):
-    df_matches = con.execute("SELECT * FROM matches;").pl()
+def _(con, pl):
+    df_matches = con.execute("SELECT * FROM matches;").pl().with_columns(
+        pl.col("from_icd_code").str.len_chars().alias("code_length")
+    )
     df_matches.head()
     return (df_matches,)
 
@@ -37,7 +39,6 @@ def _(mo):
 
     * ICD-10-GM/ICD-10-CM: chapters are not included in the OMOP voca, only from 3-7 length.
     * ICD-10-CM: the XML file didn't contain all 7-char codes available.
-
     """
     )
     return
@@ -114,13 +115,6 @@ def get_f1_score(specificity, sensitivity):
         return None
 
 
-@app.function
-def get_youndes_j(specificity, sensitivity):
-    if not specificity or not sensitivity:
-        return
-    return sensitivity + specificity - 1
-
-
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""## Test validation with matches using ICD-10-GM""")
@@ -138,8 +132,6 @@ def _(df_matches, pl):
         pl.col("to_version").eq("icd-10-who"),
         pl.col("model").eq(_model_name),
         pl.col("threshold").eq(_threshold),
-    ).with_columns(
-        pl.col("from_icd_code").str.len_chars().alias("code_length")
     )
     gm_df
     return (gm_df,)
@@ -152,7 +144,7 @@ def _(mo):
 
 
 @app.cell
-def _(OMOP_NAMING_MAPPING, gm_df, mapping_df, pl):
+def _(OMOP_NAMING_MAPPING, get_youndes_j, gm_df, mapping_df, pl):
     _filtered = mapping_df.filter(
         pl.col("vocabulary_id").eq(OMOP_NAMING_MAPPING["icd-10-gm"]),
         pl.col("vocabulary_id_icd10who").eq(OMOP_NAMING_MAPPING["icd-10-who"])
@@ -188,7 +180,6 @@ def _(OMOP_NAMING_MAPPING, gm_df, mapping_df, pl):
         _f1_score = get_f1_score(_specificity, _sensitivity)
         _youdens_j = get_youndes_j(_specificity, _sensitivity)
         print(f'Sensitivity: {_sensitivity} Specificity: {_specificity} Youden\'s J statistic: {_youdens_j} F1 score: {_f1_score}')
-
     return
 
 
@@ -199,7 +190,7 @@ def _(mo):
 
 
 @app.cell
-def _(OMOP_NAMING_MAPPING, gm_df, mapping_df, pl):
+def _(OMOP_NAMING_MAPPING, get_youndes_j, gm_df, mapping_df, pl):
     _filtered = mapping_df.filter(
         pl.col("vocabulary_id").eq(OMOP_NAMING_MAPPING["icd-10-gm"]),
         pl.col("vocabulary_id_icd10who").eq(OMOP_NAMING_MAPPING["icd-10-who"])
@@ -241,6 +232,177 @@ def _(OMOP_NAMING_MAPPING, gm_df, mapping_df, pl):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r""" """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Validation""")
+    return
+
+
+@app.cell
+def _():
+    evaluated_code_chars = {
+        "icd-10-cm": [3, 4, 5, 6],
+        "icd-10-gm": [3, 4, 5, 6, 7]
+    }
+    return (evaluated_code_chars,)
+
+
+@app.cell
+def _(df_matches, evaluated_code_chars, pl):
+    def filter_matches_by(version):
+        return df_matches.filter(
+            pl.col("from_version").eq(version),
+            pl.col("to_version").eq("icd-10-who"),
+        ).filter(pl.col("code_length").is_in(evaluated_code_chars[version]))
+    return (filter_matches_by,)
+
+
+@app.cell
+def _(OMOP_NAMING_MAPPING, mapping_df, pl):
+    def filter_omop_vocabulary_by(version):
+        return mapping_df.filter(
+            pl.col("vocabulary_id").eq(OMOP_NAMING_MAPPING[version]),
+            pl.col("vocabulary_id_icd10who").eq(OMOP_NAMING_MAPPING["icd-10-who"])
+        )
+    return (filter_omop_vocabulary_by,)
+
+
+@app.cell
+def _(filter_matches_by):
+    for (_m, _t), _df in filter_matches_by("icd-10-gm").group_by(["model", "threshold"]):
+        print(_m, _t, _df.shape)
+    return
+
+
+@app.cell
+def _(filter_matches_by, filter_omop_vocabulary_by, pl):
+    def calculate_metrics_by(version):
+        _omop_filtered = filter_omop_vocabulary_by(version)
+        _matches = filter_matches_by(version)
+
+        _results_df = {
+            "model": [],
+            "threshold": [],
+            "true_positive": [],
+            "true_negative": [],
+            "false_positive": [],
+            "false_negative": [],
+            "sensitivity": [],
+            "specificity": [],
+            "f1_score": [],
+        }
+
+
+        for (_model, _threshold), _filtered in _matches.group_by(["model", "threshold"]):
+            _results = {
+                "true_positive": 0,
+                "true_negative": 0,
+                "false_positive": 0,
+                "false_negative": 0,
+            }
+
+            for _row in _filtered.iter_rows(named=True):
+                _found = _omop_filtered.filter(
+                    pl.col("concept_code").eq(_row["from_icd_code"])
+                )
+                _not_found = _found.is_empty()
+                if _not_found:
+                    if _row["is_match"]:
+                        _results["false_positive"] += 1
+                    else:
+                        _results["true_negative"] += 1
+                else:  # found
+                    if _row["is_match"]:
+                        _results["true_positive"] += 1
+                    else:
+                        _results["false_negative"] += 1
+            _sensitivity = get_sensitivity(_results["true_positive"], _results["false_negative"])
+            _specificity = get_specificity(_results["true_negative"], _results["false_positive"])
+            _f1_score = get_f1_score(_specificity, _sensitivity)
+            _results_df["model"].append(_model)
+            _results_df["threshold"].append(_threshold)
+            _results_df["true_positive"].append(_results["true_positive"])
+            _results_df["true_negative"].append(_results["true_negative"])
+            _results_df["false_positive"].append(_results["false_positive"])
+            _results_df["false_negative"].append(_results["false_negative"])
+            _results_df["sensitivity"].append(_sensitivity)
+            _results_df["specificity"].append(_specificity)
+            _results_df["f1_score"].append(_f1_score)
+
+        return pl.DataFrame(_results_df)
+    return (calculate_metrics_by,)
+
+
+@app.cell
+def _(calculate_metrics_by):
+    metrics_gm = calculate_metrics_by("icd-10-gm")
+    return (metrics_gm,)
+
+
+@app.cell
+def _(metrics_gm):
+    metrics_gm
+    return
+
+
+@app.cell
+def _(pl):
+    def best_metrics_per_model_threshold(metrics_df):
+        return (
+            metrics_df.group_by(["model"])
+            .agg([
+                pl.col("threshold").filter(pl.col("sensitivity") == pl.col("sensitivity").max()).first().alias("best_threshold_sensitivity"),
+                pl.col("sensitivity").max().alias("best_sensitivity"),
+                pl.col("threshold").filter(pl.col("specificity") == pl.col("specificity").max()).first().alias("best_threshold_specificity"),
+                pl.col("specificity").max().alias("best_specificity"),
+                pl.col("threshold").filter(pl.col("f1_score") == pl.col("f1_score").max()).first().alias("best_threshold_f1"),
+                pl.col("f1_score").max().alias("best_f1_score")
+            ])
+        )
+    return (best_metrics_per_model_threshold,)
+
+
+@app.cell
+def _(best_metrics_per_model_threshold, metrics_gm):
+    best_metrics_per_model_threshold(metrics_gm)
+    return
+
+
+@app.cell
+def _():
+    # metrics_cm = calculate_metrics_by("icd-10-cm")
+    return
+
+
+@app.cell
+def _():
+    # best_metrics_per_model_threshold(metrics_cm)
+    return
+
+
+@app.cell
+def _(metrics_gm, px):
+    _fig_thresh = px.scatter(
+        metrics_gm,
+        x='threshold',
+        y=['false_positive', 'true_positive'], # Plot both rates
+        title='TPR and FPR at every threshold',
+        log_y=True,
+        width=700,
+        height=500
+    )
+
+    _fig_thresh.update_yaxes(title='Number of codes', rangemode='tozero')
+
+    _fig_thresh.show()
+    return
+
+
+@app.cell
+def _():
     return
 
 
